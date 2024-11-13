322a29cedc06b68524f5a657a707808eb41cc7be
Enis Kaner <enis.d.kaner@gmail.com>
2024-11-13 18:39:30 +0300

    add: SatelliteDetailFragment for ui detail of satellite

---
+++ feature/main/satellitedetail/src/main/java/com/eniskaner/satellitedetail/ui/view/SatelliteDetailFragment.kt
@@ -0,0 +1,33 @@
+package com.eniskaner.satellitedetail.ui.view
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.eniskaner.common.util.parcelable
+import com.eniskaner.satellitecommunicator.SatelliteFeatureCommunicator
+import com.eniskaner.satellitedetail.databinding.FragmentSatelliteDetailBinding
+
+
+class SatelliteDetailFragment : Fragment() {
+
+    lateinit var binding: FragmentSatelliteDetailBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentSatelliteDetailBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val args = arguments?.parcelable(SatelliteFeatureCommunicator.SATELLITE_FEATURE_NAV_KEY) as? SatelliteFeatureCommunicator.SatelliteFeatureArgs
+        val satelliteId = args?.satelliteId
+        binding.tvSatelliteDetail.text = satelliteId.toString()
+
+    }
+}
